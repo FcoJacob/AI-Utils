cdb3104b4b5337439a4c718840d8913d4bf942d4
JSarmientoP <jsarmientos@redprogrentis.com>
2024-11-09 23:56:42 +0000

    refactor ♻️(config): unused vue dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [dts({ insertTypesEntry: true }), vue(), vueDevTools()],
+  plugins: [dts({ insertTypesEntry: true }), vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
